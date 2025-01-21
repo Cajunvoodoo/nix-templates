e841577c5c1adea5761c507d02897e922f831a88
Cajunvoodoo <27892784+Cajunvoodoo@users.noreply.github.com>
2025-01-21 14:02:56 -0500

    Add aarch64-linux to C dev environment

--- C/flake.nix
+++ C/flake.nix
@@ -8,7 +8,7 @@
 
   outputs = inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       perSystem = { pkgs, system, ... }: {
         # This sets `pkgs` to a nixpkgs with allowUnfree option set.
         _module.args.pkgs = import nixpkgs {
